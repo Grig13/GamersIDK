38c2b319820b4ee7875eadeed75139a86eb7919f
Grig13 <grig.mazureanu@gmail.com>
2023-04-24 19:04:05 +0300

    changes in db

--- GamersChatAPI/Models/News.cs
+++ GamersChatAPI/Models/News.cs
@@ -2,7 +2,10 @@
 {
     public class News
     {
+        private string title;
+
         public Guid Id { get; set; }
+        public string Title { get => title; set => title = value; }
         public string Content { get; set; }
         public string? Image { get; set; }
         public string? Attachment { get; set; }
